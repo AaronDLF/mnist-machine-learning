646e9714c217297481393c68892117f351bfb650
AaronDLF <aaron10lopfortich@gmail.com>
2025-12-21 21:05:49 -0500

    Add split_data function

--- src/data_handler.cc
+++ src/data_handler.cc
@@ -15,7 +15,7 @@ data_handler::~data_handler(){
 
 void data_handler::read_feature_vector(std::string path)
 {
-  uint32_t header[4];
+  uint32_t header[4];// magic number, number of images, rows, columns
   unsigned char bytes[4];
   FILE *f = fopen(path.c_str(),"r");
   if(f)
@@ -27,7 +27,7 @@ void data_handler::read_feature_vector(std::string path)
         header[i] = convert_to_little_endian(bytes);
       }
     }
-    printf("Done with the file header\n");
+    printf("Done with the input file header\n");
 
     int image_size = header[2] * header[3];
     for (int i = 0; i < header[1]; i++)
@@ -57,11 +57,101 @@ void data_handler::read_feature_vector(std::string path)
 
 void data_handler::read_feature_labels(std::string path)
 {
+  uint32_t header[2]; // Magic number and number of items
+  unsigned char bytes[2];
+  FILE *f = fopen(path.c_str(),"r");
+  if(f)
+  {
+    for (int i = 0; i < 2; i++)
+    {
+      if(fread(bytes, sizeof(bytes),1,f))
+      {
+        header[i] = convert_to_little_endian(bytes);
+      }
+    }
+    printf("Done with the label file header\n");
 
+    for (int i = 0; i < header[1]; i++)
+    {
+      uint8_t element[1];
+
+      if (fread(element, sizeof(element), 1, f))
+      {
+        data_array -> at(i) -> set_label(element[0]);
+      }else
+      {
+        printf("Error reading the info in the file\n");
+        exit(1);
+      }
+    }
+    printf("Succesfully read the file and stored labels.\n", data_array -> size());
+  } else
+  {
+    printf("Error finding this file");
+    exit(1);
+  }
 }
 
-void data_handler::split_data();
-void data_handler::count_classes();
+void data_handler::split_data()
+{
+  std::unordered_set<int> used_indices;
+  int train_size = data_array -> size() * TRAIN_SET_PERCENT;
+  int test_size = data_array -> size() * TEST_SET_PERCENT;
+  int validation_size = data_array -> size() * VALIDATION_SET_PERCENT;
+
+  // Training set
+  int count = 0;
+
+  while (count < train_size)
+  {
+    int rand_index = rand() % data_array -> size();
+    if (used_indices.find(rand_index) == used_indices.end())
+    {
+      train_data ->push_back(data_array -> at(rand_index));
+      used_indices.insert(rand_index);
+      count ++;
+    }
+  }
+
+
+  // Test set
+
+  count = 0;
+
+  while (count < test_size)
+  {
+    int rand_index = rand() % data_array -> size();
+    if (used_indices.find(rand_index) == used_indices.end())
+    {
+      test_data ->push_back(data_array -> at(rand_index));
+      used_indices.insert(rand_index);
+      count ++;
+    }
+  }
+
+  // Validation set
+  count = 0;
+
+  while (count < validation_size)
+  {
+    int rand_index = rand() % data_array -> size();
+    if (used_indices.find(rand_index) == used_indices.end())
+    {
+      validation_data ->push_back(data_array -> at(rand_index));
+      used_indices.insert(rand_index);
+      count ++;
+    }
+  }
+
+  printf ("Training data size: %lu.\n"), train_data -> size();
+  printf ("Test data size: %lu.\n"), test_data -> size();
+  printf ("Validation data size: %lu.\n"), validation_data -> size();
+}
+
+void data_handler::count_classes()
+{
+  
+}
 
 uint32_t data_handler::convert_to_little_endian(const unsigned char* bytes);
 
