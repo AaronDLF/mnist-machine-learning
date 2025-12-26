b1f2da3e65b643e01c967e9c76dd80ed7e58b742
AaronDLF <aaron10lopfortich@gmail.com>
2025-12-26 00:36:08 -0500

    Validate performance for knn

--- KNN/src/knn.cc
+++ KNN/src/knn.cc
@@ -117,7 +117,38 @@ void knn::set_k(int val)
   k = val;
 }
 
-int knn::predict();
+int knn::predict()
+{
+  std::map<uint8_t, int> class_freq;
+
+  // Count the occurrences of each label among the k nearest neighbors
+  for (int i = 0; i < neighbors -> size(); i++)
+  {
+    if (class_freq.find(neighbors -> at(i) -> get_label()) == class_freq.end())
+    {
+      class_freq[neighbors -> at(i) -> get_label()] = 1;
+    }
+    else
+    {
+      class_freq[neighbors -> at(i) -> get_label()] += 1;
+    }
+  }
+
+  int best = 0;
+  int max = 0;
+
+  for (auto kv: class_freq)
+  {
+    if (kv.second > max)
+    {
+      max = kv.second;
+      best = kv.first;
+    }
+  }
+  delete neighbors;
+  return best;
+}
+
 double knn::calculate_distance(data * querypoint, data * input)
 {
   double distance = 0.0;
@@ -140,5 +171,25 @@ double knn::calculate_distance(data * querypoint, data * input)
   return distance;
 }
 
-double knn::validate_performance();
+double knn::validate_performance()
+{
+  double current_performance = 0;
+  int count = 0;
+  int data_index = 0;
+  for(data * query_point : *validation_data)
+  {
+    find_knearest(query_point);
+    int predicted_label = predict();
+    if (predicted_label == query_point -> get_label())
+    {
+      count++;
+    }
+    data_index++;
+  }
+  current_performance = (double)count * 100.0 / (double)data_index;
+  printf("Validation Accuracy: %.2f %%\n", current_performance);
+  return current_performance;
+
+}
+
 double knn::test_performance();
